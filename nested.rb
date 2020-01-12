3a0537c32ad46f583025767180ae98e438f3ab21
Damely Tineo <damely.tineo07@gmail.com>
2020-01-12 01:18:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8a6185341546e7b5d4ee76a85e524c521ea9e454
   return programmer_hash[:grace_hopper]
 end
 
@@ -35,7 +38,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
  
+=======
+>>>>>>> 8a6185341546e7b5d4ee76a85e524c521ea9e454
   return programmer_hash[:alan_kay][:known_for]
 end
 
@@ -55,8 +61,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
   
   return programmer_hash[:dennis_ritchie][:languages] = "C"
+=======
+  answer = programmer_hash[:dennis_ritchie][:languages]
+  return answer.to_s
+>>>>>>> 8a6185341546e7b5d4ee76a85e524c521ea9e454
 end
 
 def adding_matz
@@ -109,8 +120,13 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
   programmer_hash[:alan_kay][:known_for] = "GUI"
+<<<<<<< HEAD
 
   return programmer_hash
+=======
+  return programmer_hash
+     
+>>>>>>> 8a6185341546e7b5d4ee76a85e524c521ea9e454
 end
 
 def adding_to_dennis
@@ -130,6 +146,9 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+<<<<<<< HEAD
  
+=======
+>>>>>>> 8a6185341546e7b5d4ee76a85e524c521ea9e454
   return programmer_hash
 end
